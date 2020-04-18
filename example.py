42c1678350e650b63696fcc745135f4cf1891b55
Grisel Alpanes <griselmelina@gmail.com>
2020-04-18 16:41:05 +0100

    new files examples

--- example.py
+++ example.py
@@ -4,6 +4,9 @@ input_name = input("What's your name?\n")
 def hello(name):
     print("hello! " + name)
 
+def bye(name):
+    print("Bye bye " + name)
 
 print('Hi ' + input_name)
 hello(input_name)
+bye(input_name)
